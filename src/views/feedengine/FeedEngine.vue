bb24bab29ccd8a1e0b10e91e647a84481e79d1e9
Fabrice-Simo <j.fabricef.simo@gmail.com>
2022-02-28 11:26:32 +0100

    components: feed engine container created

--- src/views/feedengine/FeedEngine.vue
+++ src/views/feedengine/FeedEngine.vue
@@ -2,6 +2,10 @@
   <div class="test">
     <h1 style="color: gray">{{ msg }}</h1>
     <fe-button @mbOnClick="greet">add album</fe-button>
+    <div class="fe-container">
+      <div class="fe-album-list"><h1>Just a</h1></div>
+      <div class="fe-photo-list"><h1>quick test</h1></div>
+    </div>
   </div>
 </template>
 
@@ -37,7 +41,35 @@ export default {
 
 <style scoped>
 @import url("@/styleGuide/index.css");
-.test {
+.fe-container {
+  position: relative;
+
+  margin: 0 auto;
+  padding: var(--fe-small-spacing);
+  border-radius: var(--fe-medium-spacing);
+  color: var(--fe-accent);
+  width: var(--fe-half-width);
+
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
   background-color: var(--fe-primary);
+  opacity: 1;
+  background-image: radial-gradient(
+    var(--fe-primary-ligth) 0.45px,
+    var(--fe-primary) 0.45px
+  );
+  background-size: 9px 9px;
+
+  display: grid;
+  grid-template-areas: "fe-album-list fe-album-list . . . . fe-photo-list  fe-photo-list";
+}
+
+.fe-album-list {
+  grid-area: fe-album-list;
+  background-color: red;
+}
+
+.fe-photo-list {
+  grid-area: fe-photo-list;
+  background-color: green;
 }
 </style>
